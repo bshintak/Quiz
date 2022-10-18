79661f4f6b42c5f7b1c1e5f432da744b69419ead
Beatriz Schileo Shintaku <87424758+bshintak@users.noreply.github.com>
2022-10-18 18:59:45 +0100

    Add files via upload

--- game.js
+++ game.js
@@ -1,20 +1,45 @@
-function wrong (no)
-{
-	if (no == 1)
-		document.getElementById("incorrect1").style.background = "red";
-	else if (no == 2)
-		document.getElementById("incorrect2").style.background = "red";
-	else if (no == 3)	
-		document.getElementById("incorrect3").style.background = "red";
-};
+let buttonsValue = new Array;
+// let result = 0;
+// window.localStorage.setItem("result", result);
+// window.localStorage.getItem("result");
+// localStorage.setItem("result", result);
+// const result = localStorage.getItem("result");
+let indexCorrect = document.querySelector(".correctIdx").textContent;
+const buttons = document.querySelectorAll("button");
 
-
-function right ()
-{
-	document.getElementById("correct").style.background = "green";
-	console.log("asdfg");
-	sessionStorage.setItem('correct', '1');
-	console.log(sessionStorage.getItem('correct'));
+const initGame = function(indexCorrect) {
+	for(let i = 0; i < 4; i++) {
+		if (i == indexCorrect)
+			buttonsValue.push(true);
+		else 
+			buttonsValue.push(false);	
+	}
 }
 
+const disableButtons = function() {
+	for(let i = 0; i < 4; i++) {
+		// buttons[i].disabled = true;
+		buttons[i].classList.add("avoid-clicks");
+	}
+}
 
+const enableButtons = function() {
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener("click", function() {
+			if (buttonsValue[i] == true){
+				buttons[i].classList.add("correct");
+				localStorage.clickcount += 1;
+				localStorage.removeItem(localStorage.clickcount);
+				console.log(localStorage.clickcount);
+				// document.querySelector("score").textContent = sessionStorage.clickcount;
+				// result++;
+			}
+			else
+				buttons[i].classList.add("wrong");
+			disableButtons();
+		});
+	}
+}
+
+initGame(indexCorrect);
+enableButtons();
